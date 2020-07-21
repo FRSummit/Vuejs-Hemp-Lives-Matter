e3bd0a8563046c24adbbaa9c6293654dd0ba7c1f
frsummit@simplexhub.com <www.frsummit1@gmail.com>
2020-07-21 20:24:02 +0600

    hlm featured works properly

--- src/components/product/HLMFeatures.vue
+++ src/components/product/HLMFeatures.vue
@@ -1,27 +1,132 @@
 <template>
-  <div class="hlm-feature">
+  <div class="hlm-feature" id="hlm-feature">
     <p class="title">H.L.M. FEATURED</p>
-    <div class="product-section" v-for="(item, i) in items" :key="i">
+
+    <!-- Progressbar -->
+    <v-progress-circular
+      v-if="!progressBar"
+      class="v-progress-circular"
+      :size="70"
+      :width="7"
+      color="purple"
+      indeterminate
+    ></v-progress-circular>
+
+    <!-- Products -->
+    <div class="product-section" v-for="(item, i) in productList" :key="i">
+      <div class="remove-product-section" @click="removeItem(i)" v-if="userIsAuthenticated">
+        <img src="../../assets/images/trush.png" alt="trush">
+      </div>
       <div class="product-img-sec">
-        <img :src="item.src" alt />
+        <img :src="item.img1_url" alt />
       </div>
       <div class="product-txt-btn">
-        <p>{{ item.details }}</p>
+        <p>{{ item.title }}</p>
         <div class="learn-more-btn">
-          <a href="https://hemplivesmatter.com/products/" target="_blank">
+          <a :href="item.productUrl" target="_blank">
             <span class="elementor-button-text">Learn More</span>
           </a>
         </div>
       </div>
     </div>
+
+    <!-- Add Product Section -->
+    <div class="add-new-btn" v-if="userIsAuthenticated && productListLength<3">
+      <a @click="addNewProduct">
+        <span class="elementor-button-text">{{ addNewBlogIndex }}</span>
+      </a>
+    </div>
+    <!-- Form -->
+    <v-card class="elevation-12 v-card-edit" v-if="userIsAuthenticated && createNewBlogIndex && productListLength<3">
+      <v-toolbar color="primary" dark flat>
+        <v-spacer />
+        <v-toolbar-title>New Product</v-toolbar-title>
+        <v-spacer />
+      </v-toolbar>
+      <v-card-text class="v-card-text">
+        <v-form @submit.prevent="onSubmit">
+          <!-- title & productUrl -->
+          <div class="name-field">
+            <label class="field-label">Name of your feature</label>
+            <v-text-field
+              class="field-input"
+              v-model="title"
+              id="title"
+              placeholder="Name of your feature"
+              type="text"
+            />
+          </div>
+          <div class="name-field">
+            <label class="field-label">Feature Url</label>
+            <v-text-field
+              class="field-input"
+              v-model="productUrl"
+              id="productUrl"
+              placeholder="Feature url"
+              type="text"
+            />
+          </div>
+
+          <!-- img 1 -->
+          <div class="name-field">
+            <label class="field-label img-sec">Upload an image:</label>
+            <input
+              class="field-input"
+              @change="previewImage"
+              id="img1"
+              type="file"
+              accept="image/*"
+            />
+            <div style="display: inline-block;">
+              <p>
+                Progress: {{uploadValue.toFixed()+"%"}}
+                <progress
+                  id="progress"
+                  :value="uploadValue"
+                  max="100"
+                ></progress>
+              </p>
+            </div>
+            <div v-if="imageData!=null">
+              <img class="preview" :src="picture" style="width: 200px;" />
+              <br />
+              <span @click="onUpload" class="img-up-btn">Upload</span>
+            </div>
+          </div>
+
+          <!-- submit section -->
+          <v-card-actions>
+            <v-spacer />
+            <v-btn type="submit" class="create-btn" color="primary">Create</v-btn>
+            <v-spacer />
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+    <!-- Form -->
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   // name: "HLM Feature",
   data() {
     return {
+      userIsAuthenticated: false,
+      addNewBlogIndex: "Add New Featured",
+      createNewBlogIndex: false,
+      title: null,
+      productUrl: null,
+      img1_url: null, //For image upload
+      picture: null, //For image upload
+      imageData: null, //For image upload
+      uploadValue: 0, //For image upload
+      productList: [],
+      progressBar: false,
+      productListLength: null,
+      minFeatureReachBtn: true,
       items: [
         {
           src: require("../../assets/images/productsHempGummies-orvmua8mk78logs7zk2mm68out4rh8pcy5tl451rt4.png"),
@@ -37,6 +142,108 @@ export default {
         }
       ]
     };
+  },
+  created() {
+    const usrAuth = localStorage.getItem("admin_info_hlm");
+    if (usrAuth) {
+      this.userIsAuthenticated = JSON.parse(usrAuth).auth_hlm;
+    }
+    // load products
+    firebase
+      .database()
+      .ref("hlm_product_feature_list")
+      .on("value", snapshot => {
+        this.productList = snapshot.val();
+        this.progressBar = true
+        this.productListLength = Object.keys(snapshot.val()).length
+      });
+  },
+  methods: {
+    addNewProduct() {
+      if (!this.createNewBlogIndex) {
+        this.createNewBlogIndex = true;
+        this.addNewBlogIndex = "Close";
+        document.querySelector(".add-new-btn a").style.background = "red";
+        document.querySelector(".add-new-btn a").style.color = "#fff";
+      } else if (this.createNewBlogIndex) {
+        this.createNewBlogIndex = false;
+        this.addNewBlogIndex = "Add New Featured";
+        document.querySelector(".add-new-btn a").style.background =
+          "#F3C12A";
+        document.querySelector(".add-new-btn a").style.color = "#000";
+      }
+    },
+    // Adding new content
+    onSubmit() {
+      if (
+        this.title === "" ||
+        this.title === null ||
+        this.productUrl === "" ||
+        this.productUrl === null
+      ) {
+        console.log("null");
+        if (this.title == "" || this.title === null) {
+          document.getElementById("title").placeholder =
+            "Title must not be empty";
+        }
+        if (this.productUrl == "" || this.productUrl === null) {
+          document.getElementById("productUrl").placeholder =
+            "Product url must not be empty";
+        }
+      } else {
+        firebase
+          .database()
+          .ref("hlm_product_feature_list")
+          .push({
+            title: this.title,
+            productUrl: this.productUrl,
+            img1_url: this.img1_url
+          })
+          .then(data => {
+            console.log(data.path.pieces_[1]);
+            (this.title = ""), (this.productUrl = "");
+          })
+          .catch(error => console.log(error));
+      }
+    },
+    onUpload() {
+      //For image upload
+      this.picture = null;
+      const storageRef = firebase
+        .storage()
+        .ref(`${this.imageData.name}`)
+        .put(this.imageData);
+      storageRef.on(
+        `state_changed`,
+        snapshot => {
+          this.uploadValue =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        error => {
+          console.log(error.message);
+        },
+        () => {
+          this.uploadValue = 100;
+          storageRef.snapshot.ref.getDownloadURL().then(url => {
+            this.picture = url;
+            this.img1_url = url;
+            console.log(this.img1_url);
+          });
+        }
+      );
+    },
+    previewImage(event) {
+      //For image upload
+      this.uploadValue = 0;
+      this.picture = null;
+      this.imageData = event.target.files[0];
+    },
+    removeItem(key) {
+      firebase
+        .database()
+        .ref("hlm_product_feature_list/" + key)
+        .remove();
+    }
   }
 };
 </script>
@@ -44,6 +251,7 @@ export default {
 <style scoped>
 .hlm-feature {
   text-align: center;
+  position: relative;
 }
 .title {
   font-family: "Montserrat", Sans-serif;
@@ -64,6 +272,17 @@ export default {
   width: 22%;
   display: inline-block;
   margin: 0 60px;
+  position: relative;
+}
+.remove-product-section {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+}
+.remove-product-section img {
+  width: 20px;
+  height: 20px;
 }
 .product-img-sec {
   width: 100%;
@@ -103,5 +322,95 @@ export default {
 }
 .learn-more-btn a:hover {
   background-color: #ff1700;
+}
+
+/* Add new Button */
+.add-new-btn {
+  text-align: right;
+  margin: 0 40px;
+  position: absolute;
+    top: 16px;
+    right: 20px;
+}
+.add-new-btn a {
+  color: #000;
+  padding: 12px 40px;
+  display: inline-block;
+  text-transform: uppercase;
+  font-size: 16px;
+  letter-spacing: 1px;
+  text-decoration: none;
+  text-transform: uppercase;
+  text-align: center;
+  border-radius: 60px !important;
+  font-weight: bold;
+  font-family: Arial, Helvetica, sans-serif;
+  cursor: pointer;
+  border-radius: 6px;
+  width: 100%;
+  font-weight: bold;
+  cursor: pointer;
+  background: #F3C12A;
+  border: 2px solid #3b8a00;
+}
+
+/* Form / v-card */
+#hlm-feature .v-sheet.v-card {
+    width: 50%;
+    margin: 0 auto;
+    position: absolute;
+    top: 20px;
+    left: 40px;
+    z-index: 5;
+}
+.v-card-text {
+  width: 100%;
+  padding: 20px;
+  clear: both;
+  box-shadow: inset 0 10px 0 0 rgba(0, 0, 0, 0.1);
+  background: #F3C02A;
+  position: relative;
+  overflow: hidden;
+    border: 2px solid #000;
+}
+.add-new-for-index {
+  width: 60%;
+  margin: 0 auto;
+}
+.name-field {
+  text-align: left;
+}
+label {
+  font-size: 20px;
+  font-weight: bold;
+  color: #000;
+}
+.img-sec {
+  width: 30%;
+}
+input {
+  color: #000;
+  margin-left: 10px;
+  width: 30%;
+}
+.img-up-btn,
+.create-btn {
+  color: #000;
+  padding: 4px 12px;
+  display: inline-block;
+  font-size: 12px;
+  letter-spacing: 1px;
+  text-decoration: none;
+  text-transform: uppercase;
+  border: 2px #000 solid;
+  text-align: center;
+  border-radius: 6px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.create-btn {
+  color: #000 !important;
+  padding: 4px 12px !important;
+  background: transparent !important;
 }
 </style>
